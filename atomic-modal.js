08ac7979338905d9d92df555cc2aba4541cf40c1
Anton Staroverov <tonystarring@gmail.com>
2018-05-16 14:12:55 +0200

    Helper class on `body`

--- atomic-modal.js
+++ atomic-modal.js
@@ -2,7 +2,15 @@
 
 function initModal($, context) {
   var defaults = {
-    type: 'inline'
+    type: 'inline',
+    callbacks: {
+      open: function() {
+        $('body').addClass('open-modal');
+      },
+      close: function() {
+        $('body').removeClass('open-modal');
+      }
+    }
   };
 
   $(context).find('[data-modal]').each(function() {
